5c3c81b3e3c47061ce5418774fc122c89bc50a2c
afarica <57631090+afarica@users.noreply.github.com>
2019-11-12 03:35:33 +0600

    Update p28.py

--- p28.py
+++ p28.py
@@ -1,3 +1,8 @@
+# In mathematics, the factorial of an integer n, denoted by n! is the following
+# product:
+# n!=1×2×...×n
+# For the given integer n
+# calculate the value n!. Don't use math module in this exercise.
 a=int(input("Enter your number:"))
 i=1 
 b=1
